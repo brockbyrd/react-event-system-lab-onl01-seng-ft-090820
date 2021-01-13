f882ba01a9fe40d7de2e9f052eb4ed6e9f4fc395
brockbyrd <brockbyrdd@gmail.com>
2021-01-13 16:37:07 -0600

    commit tree 2de42a30a14fcfa0139fb1782f18ff35728ed5a8

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class EyesOnMe extends Component {
+
+    constructor(){
+
+    }
+
+    render() {
+        console.log("This is the eyesonme component")
+    }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+// Code EyesOnMe Component Here
+import React, { Component } from 'react'
+
+export default class Keypad extends Component {
+
+    constructor(){
+
+    }
+
+    render() {
+        console.log("This is the keypad component")
+    }
+}
